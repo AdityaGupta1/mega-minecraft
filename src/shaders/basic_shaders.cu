3e125fcd405bb3e40ccb783e26dd85940f0a5a1b
Aditya Gupta <adityag@gmail.com>
2023-12-02 22:11:07 -0500

    reorganize things

--- src/shaders/basic_shaders.cu
+++ src/shaders/basic_shaders.cu
@@ -318,7 +318,103 @@ float3 refract(float3 wo, float3 n, float eta) {
     }
 }
 
-static __forceinline__ __device__ 
+// noise code from here: https://forum.pjrc.com/index.php?threads/im-looking-for-a-performant-perlin-or-open-simplex-noise-implementation.72409/post-322480
+__constant__ unsigned char permutation[] = {
+     151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103,
+     30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197,
+     62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20,
+     125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231,
+     83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102,
+     143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200,
+     196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226,
+     250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16,
+     58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221,
+     153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232,
+     178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179,
+     162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157,
+     184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114,
+     67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
+};
+
+__device__ float fade(float t) { return t * t * t * (t * (t * 6 - 15) + 10); }
+__device__ float lerp2(float t, float a, float b) { return a + t * (b - a); }
+__device__ float grad(int hash, float x, float y, float z)
+{
+    // convert lower 4 bits of hash code into 12 gradient directions
+    int    h = hash & 15;
+    float  u = h < 8 ? x : y,
+        v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+}
+
+#define P(x) permutation[(x) & 255]
+
+__device__ float pnoise(float3 p)
+{
+    // find unit cube containing point
+    int X = (int)floorf(p.x) & 255,
+        Y = (int)floorf(p.y) & 255,
+        Z = (int)floorf(p.z) & 255;
+
+    // find relative x, y, z, of each point in cube
+    p.x -= floorf(p.x);
+    p.y -= floorf(p.y);
+    p.z -= floorf(p.z);
+
+    // compute fade curves for each of x, y, z
+    float u = fade(p.x),
+        v = fade(p.y),
+        w = fade(p.z);
+
+    // hash coordinates of 8 cube corners
+    int A = P(X) + Y,
+        AA = P(A) + Z,
+        AB = P(A + 1) + Z,
+        B = P(X + 1) + Y,
+        BA = P(B) + Z,
+        BB = P(B + 1) + Z;
+
+    // add blended results from 8 corners of cube
+    return
+        lerp2(w,
+            lerp2(v,
+                lerp2(u,
+                    grad(P(AA), p.x, p.y, p.z),
+                    grad(P(BA), p.x - 1, p.y, p.z)),
+                lerp2(u,
+                    grad(P(AB), p.x, p.y - 1, p.z),
+                    grad(P(BB), p.x - 1, p.y - 1, p.z))),
+            lerp2(v,
+                lerp2(u,
+                    grad(P(AA + 1), p.x, p.y, p.z - 1),
+                    grad(P(BA + 1), p.x - 1, p.y, p.z - 1)),
+                lerp2(u,
+                    grad(P(AB + 1), p.x, p.y - 1, p.z - 1),
+                    grad(P(BB + 1), p.x - 1, p.y - 1, p.z - 1)
+                )
+            )
+        );
+}
+
+#undef P
+
+template<int octaves = 5>
+__device__
+float fbm(float3 p)
+{
+    float fbm = 0.f;
+    float amplitude = 1.f;
+#pragma unroll
+    for (int i = 0; i < octaves; ++i)
+    {
+        amplitude *= 0.5f;
+        fbm += amplitude * pnoise(p);
+        p *= 2.f;
+    }
+    return fbm;
+}
+
+static __forceinline__ __device__
 float3 sinf(float3 p)
 {
     return make_float3(__sinf(p.x), __sinf(p.y), __sinf(p.z));
@@ -490,8 +586,8 @@ float3 getSkyColor(float3 rayDir, bool& foundLightSource)
     {
         float horizontalDist = acosf(dot(make_float2(rayDir.x, rayDir.z), make_float2(params.sunDir.x, params.sunDir.z)));
         float orangeStrength = smoothstep(-0.13f, -0.02f, params.sunDir.y) * smoothstep(0.25f, 0.05f, params.sunDir.y) 
-            * smoothstep(-1.4f, 0.65f, sunD)
-            * smoothstep(1.05f, 0.18f, rayDir.y + (powf(horizontalDist, 1.4f) * 0.13f));
+            * smoothstep(-2.5f, 0.65f, sunD)
+            * smoothstep(1.05f, 0.18f, rayDir.y + (smoothstep(0.f, PI, horizontalDist) * 0.6f));
         if (orangeStrength > 0.f)
         {
             float3 orangeColor = make_float3(1.4f, 0.35f, 0.f);
@@ -531,108 +627,15 @@ static __device__ float schlickFresnel(const float3& V, const float3& N, const f
     return R0 + (1.f - R0) * pow(1.f - cosTheta, 5.f);
 }
 
-// noise code from here: https://forum.pjrc.com/index.php?threads/im-looking-for-a-performant-perlin-or-open-simplex-noise-implementation.72409/post-322480
-__constant__ unsigned char permutation[] = {
-     151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103,
-     30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197,
-     62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20,
-     125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231,
-     83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102,
-     143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200,
-     196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226,
-     250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16,
-     58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221,
-     153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232,
-     178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179,
-     162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157,
-     184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114,
-     67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-};
-
-__device__ float fade(float t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-__device__ float lerp2(float t, float a, float b) { return a + t * (b - a); }
-__device__ float grad(int hash, float x, float y, float z)
-{
-    // convert lower 4 bits of hash code into 12 gradient directions
-    int    h = hash & 15;
-    float  u = h < 8 ? x : y,
-           v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-}
-
-#define P(x) permutation[(x) & 255]
-
-__device__ float pnoise(float3 p)
-{
-    // find unit cube containing point
-    int X = (int)floorf(p.x) & 255,
-        Y = (int)floorf(p.y) & 255,
-        Z = (int)floorf(p.z) & 255;
-
-    // find relative x, y, z, of each point in cube
-    p.x -= floorf(p.x);
-    p.y -= floorf(p.y);
-    p.z -= floorf(p.z);
-
-    // compute fade curves for each of x, y, z
-    float u = fade(p.x),
-          v = fade(p.y),
-          w = fade(p.z);
-
-    // hash coordinates of 8 cube corners
-    int A  = P(X) + Y,
-        AA = P(A) + Z,
-        AB = P(A + 1) + Z,
-        B  = P(X + 1) + Y,
-        BA = P(B) + Z,
-        BB = P(B + 1) + Z;
-
-    // add blended results from 8 corners of cube
-    return
-        lerp2(w,
-            lerp2(v,
-                lerp2(u,
-                    grad(P(AA), p.x, p.y, p.z),
-                    grad(P(BA), p.x - 1, p.y, p.z)),
-                lerp2(u,
-                    grad(P(AB), p.x, p.y - 1, p.z),
-                    grad(P(BB), p.x - 1, p.y - 1, p.z))),
-            lerp2(v,
-                lerp2(u,
-                    grad(P(AA + 1), p.x, p.y, p.z - 1),
-                    grad(P(BA + 1), p.x - 1, p.y, p.z - 1)),
-                lerp2(u,
-                    grad(P(AB + 1), p.x, p.y - 1, p.z - 1),
-                    grad(P(BB + 1), p.x - 1, p.y - 1, p.z - 1)
-                )
-            )
-        );
-}
-
-#undef P
-
-float __device__ fbm(float3 p)
-{
-    float fbm = 0.f;
-    float amplitude = 1.f;
-    #pragma unroll
-    for (int i = 0; i < 4; ++i)
-    {
-        amplitude *= 0.5f;
-        fbm += amplitude * pnoise(p);
-        p *= 2.f;
-    }
-    return fbm;
-}
-
-__device__ void applyWaveNoise(const float3& pos, float3& nor)
+__device__
+void applyWaveNoise(const float3& pos, float3& nor)
 {
     //float3 noisePos = make_float3(pos.x + pos.y, pos.z + pos.y, params.time);
     float3 noisePos = pos;
     noisePos.x *= 0.45f;
 
-    float perlinX = fbm(noisePos);
-    float perlinZ = fbm(noisePos + make_float3(74159.21f, 21982.43f, 18923.34f));
+    float perlinX = fbm<4>(noisePos);
+    float perlinZ = fbm<4>(noisePos + make_float3(74159.21f, 21982.43f, 18923.34f));
 
     nor.x += perlinX * 0.3f;
     nor.z += perlinZ * 0.3f;
